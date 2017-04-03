f3000ad349a2a198701d965d2afc2b3dab06f88d
mbisurgi-designfreed <maximiliano@designfreed.com>
2017-04-02 22:40:41 -0300

    Implementacion LoginActivity - Google

--- app/src/main/java/com/designfreed/toowatch/LoginActivity.java
+++ app/src/main/java/com/designfreed/toowatch/LoginActivity.java
@@ -11,10 +11,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -28,10 +37,16 @@ public class LoginActivity extends AppCompatActivity {
     private Button mLoginBtn;
     private Button mSignUpBtn;
 
+    private SignInButton mLoginGoogleBtn;
+
     private DatabaseReference mDatabaseUsers;
     private FirebaseAuth mAuth;
 
+    private GoogleApiClient mGoogleApiClient;
+
     private ProgressDialog mProgress;
+
+    private static final int RC_SIGN_IN = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +69,14 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        mLoginGoogleBtn = (SignInButton) findViewById(R.id.login_google_btn);
+        mLoginGoogleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                googleSignIn();
+            }
+        });
+
         mSignUpBtn =(Button) findViewById(R.id.signup_btn);
         mSignUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,6 +86,21 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(registerIntent);
             }
         });
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+                    }
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
 
         mProgress = new ProgressDialog(this);
     }
@@ -85,11 +123,12 @@ public class LoginActivity extends AppCompatActivity {
 
                     } else {
 
+                        mProgress.dismiss();
+
                         Toast.makeText(LoginActivity.this, "Datos de ingreso incorrectos", Toast.LENGTH_LONG).show();
 
                     }
 
-                    mProgress.dismiss();
                 }
             });
 
@@ -97,33 +136,89 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void googleSignIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+
+            mProgress.setMessage("Iniciando sesion ...");
+            mProgress.show();
+
+            if (result.isSuccess()) {
+
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+
+            } else {
+
+                mProgress.dismiss();
+
+            }
+
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+
+                            mProgress.dismiss();
+
+                            Toast.makeText(LoginActivity.this, "Datos de ingreso incorrectos", Toast.LENGTH_LONG).show();
+
+                        } else {
+
+                            checkUserExist();
+
+                        }
+                    }
+                });
     }
 
     private void checkUserExist() {
-        final String userId = mAuth.getCurrentUser().getUid();
+        if (mAuth != null) {
 
-        mDatabaseUsers.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                if (dataSnapshot.hasChild(userId)) {
+            mProgress.dismiss();
 
-                    Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
-                    mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    startActivity(mainIntent);
+            final String userId = mAuth.getCurrentUser().getUid();
 
-                } else {
+            mDatabaseUsers.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.hasChild(userId)) {
 
-                    Toast.makeText(LoginActivity.this, "Debes configurar tu cuenta", Toast.LENGTH_LONG).show();
+                        Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+                        mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(mainIntent);
+
+                    } else {
+
+                        Intent setupIntent = new Intent(LoginActivity.this, SetupActivity.class);
+                        setupIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(setupIntent);
+
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
 
                 }
-            }
+            });
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
+        }
     }
 
 }
--- app/src/main/java/com/designfreed/toowatch/MainActivity.java
+++ app/src/main/java/com/designfreed/toowatch/MainActivity.java
@@ -1,13 +1,17 @@
 package com.designfreed.toowatch;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.mindorks.placeholderview.SwipePlaceHolderView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     private DatabaseReference mDatabaseUsers;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    private ProgressDialog mProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
+
+        mProgress = new ProgressDialog(this);
     }
 
     @Override
@@ -51,6 +59,40 @@ public class MainActivity extends AppCompatActivity {
         super.onStop();
 
         mAuth.removeAuthStateListener(mAuthStateListener);
+    }
+
+    private void checkUserExist() {
+        if (mAuth != null) {
+
+            mProgress.dismiss();
+
+            final String userId = mAuth.getCurrentUser().getUid();
+
+            mDatabaseUsers.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.hasChild(userId)) {
+
+                        Intent mainIntent = new Intent(MainActivity.this, MainActivity.class);
+                        mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(mainIntent);
+
+                    } else {
+
+                        Intent setupIntent = new Intent(MainActivity.this, SetupActivity.class);
+                        setupIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(setupIntent);
+
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+        }
     }
 
 }
