61f88860e912bed58e42dd25f4820dbf3560ed2c
mbisurgi-designfreed <maximiliano@designfreed.com>
2017-04-03 21:16:11 -0300

    Login interface, Create account interface

--- app/src/main/java/com/designfreed/toowatch/RegisterActivity.java
+++ app/src/main/java/com/designfreed/toowatch/RegisterActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -34,6 +35,8 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         mDatabase = FirebaseDatabase.getInstance().getReference().child("users");
 
